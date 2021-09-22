50decdcd5a1b194f2c2d85059ab8820c75084725
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-09-22 09:45:50 -0400

    Add intelly project

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,21 @@ function About() {
       <h2>A little more about me...</h2>
       <p>
         My name is Tomek Regulski - I am a full-stack developer based in
-        Manhattan. In addition to my progamming work, I have 7+ years experience
-        in CPG, and 10+ years experience in the performing arts.
+        Manhattan. In addition to my programming work, I have 7+ years
+        experience in CPG, and 10+ years experience in the performing arts.
       </p>
       <p>
         I am passionate about building elegant and intuitive solutions to the
         task at hand - whether it be optimizing workflow, connecting with an
-        audience, or discovering a new park to explore. Some of the tools that I
-        use include HTML, CSS, JavaScript, SQL, and React.
+        audience, or exploring a hobby. Some of the tools that I use include
+        HTML, CSS, JavaScript, Node.js, MySQL, MongoDB, React, Heroku, and
+        Material-UI.
       </p>
       <p>
         I am equally at home in front and back end development, and love taking
-        the journery from abstract idea to practical execution. If you have a
-        project in mind, or simply want to inquire further - please get in touch
-        through any of the methods below, and let's chat soon!
+        the journey from abstract idea to practical execution. If you have a
+        project in mind, or simply want to inquire further - get in touch and
+        let's chat soon!
       </p>
     </section>
   );
